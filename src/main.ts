bbfe25138393ca2ca6d496ac0e2697ce7754b01b
Santos <156567388+SNSCastillo@users.noreply.github.com>
2025-03-25 23:54:07 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-// Para usar la documentación de Swagger
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
-
-  // Antes que se inicie el server se pone la configuración de Swagger
+  
   const config = new DocumentBuilder()
     .setTitle('Tienda de productos')
     .setDescription('Este proyecto es para pruebas')
@@ -18,10 +15,8 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  // Configuración de CORS
-  // Esto acepta cualquier dominio el backend
   app.enableCors();
 
-  await app.listen(process.env.PORT ?? 4000); // porque el 3000 va a ser del front
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
