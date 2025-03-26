d2e55d2390ef5ebdae2d67177112e4b511a29e02
Santos <156567388+SNSCastillo@users.noreply.github.com>
2025-03-25 23:56:37 -0600

    Update create-product.dto.ts

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,4 +1,2 @@
-// export class CreateProductDto {}
 import {Product} from '@prisma/client'
-// Esto es lo que espera desde el fronted
 export type CreateProductDto = Omit<Product, "id" | "createdAt"| "updatedAt">
