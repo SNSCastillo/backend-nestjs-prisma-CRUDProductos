2d0169702b50fbf74c54e006e63922dfe58a5f64
Santos <156567388+SNSCastillo@users.noreply.github.com>
2025-03-25 23:57:47 -0600

    Update products.module.ts

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -6,6 +6,6 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Module({
   imports: [],
   controllers: [ProductsController],
-  providers: [ProductsService, PrismaService], // importante importar PrismaService
+  providers: [ProductsService, PrismaService],
 })
 export class ProductsModule {}
